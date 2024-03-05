01675e854e25e3275afc04def5c49b2291dd2597
Jani Mustonen <jani.mustonen@taitounited.fi>
2024-03-05 21:31:44 +0200

    jj: add a setup script for jj configs

---
+++ setup/jj.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env zsh
+
+set -euxo pipefail
+
+jj config set --user user.name "Jani Musronen"
+
+jj config set --user ui.paginate "never"
+jj config set --user ui.default-command log
+
+jj config set --user revsets.log \
+  '@ | trunk() | (ancestors(immutable_heads().., 2) ~ ::(remote_branches() ~ branches()))'
+
+# Finds branches being worked on by x
+jj config set --user revset-aliases."work(x)" \
+  'immutable_heads()..((immutable_heads().. & (author(x) | committer(x)))::)'
+
