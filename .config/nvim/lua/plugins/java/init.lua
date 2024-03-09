45e2c4e574fff71ddfcfc35f6e6986aa38fc2449
Jani Mustonen <jani.mustonen@taitounited.fi>
2024-03-09 12:36:59 +0200

    vim: lazy load java lsp

--- .config/nvim/lua/plugins/java/init.lua
+++ .config/nvim/lua/plugins/java/init.lua
@@ -1,6 +1,11 @@
 -- See https://github.com/nvim-java/nvim-java/issues/98#issuecomment-1871010340
 return {
   "nvim-java/nvim-java",
+
+  -- Lazy load when we open a java file
+  lazy = true,
+  ft = "java",
+
   dependencies = {
     "nvim-java/lua-async-await",
     "nvim-java/nvim-java-core",
