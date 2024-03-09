cc0c4c5afb2656295a21559818668972d62882f0
Jani Mustonen <jani.mustonen@taitounited.fi>
2024-03-09 12:41:39 +0200

    vim: better code completion behqviour

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -6,7 +6,7 @@ return {
   },
 
   -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
+  { "ellisonleao/gruvbox.nvim", lazy = false, priority = 1000 },
 
   -- Configure LazyVim to load gruvbox
   {
@@ -14,5 +14,43 @@ return {
     opts = {
       colorscheme = "gruvbox",
     },
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local cmp = require("cmp")
+
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if has_words_before() then
+            cmp.select_next_item()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      })
+
+      -- Prevent cmp from preselecting the first item, allowing us to hit Enter normally.
+      opts.preselect = cmp.PreselectMode.None
+      opts.completion.completeopt = "menu,menuone,noselect"
+
+      return opts
+    end,
   },
 }
