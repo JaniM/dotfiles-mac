c5b9b075775057c2ef95e8f3d0ae73c488649f2c
Jani Mustonen <jani.mustonen@taitounited.fi>
2024-03-20 16:53:38 +0200

    system-specific darwin configuration

--- flake.nix
+++ flake.nix
@@ -11,28 +11,31 @@
 
   outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
   let
-    mkSystem = homeModules: nix-darwin.lib.darwinSystem {
-      modules = [
-        {
-          # Set Git commit hash for darwin-version.
-          system.configurationRevision = self.rev or self.dirtyRev or null;
-        }
+    include = path: if builtins.pathExists path then path else null;
+    withoutNulls = list: builtins.filter (x: x != null) list;
+
+    mkSystem = root: nix-darwin.lib.darwinSystem {
+      modules = withoutNulls [
+        # Set Git commit hash for darwin-version.
+        { system.configurationRevision = self.rev or self.dirtyRev or null; }
+
         ./configuration.nix
-        home-manager.darwinModules.home-manager {
+        (include (root + "/configuration.nix"))
+
+        home-manager.darwinModules.home-manager
+        {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.users.janimustonen = import ./home.nix;
 
-          home-manager.extraSpecialArgs = {
-            modules = homeModules;
-          };
+          home-manager.extraSpecialArgs.modules = [(root + "/home.nix")];
         }
       ];
     };
   in {
     # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#simple
-    darwinConfigurations."jazz" = mkSystem [ ./jazz.nix ];
-    darwinConfigurations."treble" = mkSystem [ ./treble.nix ];
+    # $ darwin-rebuild build --flake .
+    darwinConfigurations."jazz" = mkSystem ./jazz;
+    darwinConfigurations."treble" = mkSystem ./treble;
   };
 }
