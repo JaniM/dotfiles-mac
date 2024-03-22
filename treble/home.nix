e5c380751f719055ab43a3b2d7d7235129e8049c
Jani Mustonen <jani.mustonen@taitounited.fi>
2024-03-22 09:36:56 +0200

    add work tools

--- treble/home.nix
+++ treble/home.nix
@@ -13,4 +13,11 @@
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
   home.stateVersion = "23.11"; # Please read the comment before changing.
+
+  home.packages = with pkgs; [
+    awscli2
+    azure-cli
+    kubectl
+    kustomize
+  ];
 }
