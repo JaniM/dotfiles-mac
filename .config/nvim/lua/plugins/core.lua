62de829f590128a5c7824cfe846b5efc5a703d63
Jani Mustonen <janijohannes@kapsi.fi>
2024-07-01 10:57:30 +0300

    restore tab behavior in vim

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -32,10 +32,21 @@ return {
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
       local cmp = require("cmp")
+
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
         ["<CR>"] = cmp.mapping.confirm({ select = false }),
         ["<Tab>"] = cmp.mapping(function(fallback)
-          cmp.select_next_item()
+          if has_words_before() then
+            cmp.select_next_item()
+          else
+            fallback()
+          end
         end, { "i", "s" }),
         ["<S-Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
